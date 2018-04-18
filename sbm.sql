132b0a6aa11b421e865e9355ad51b44a07b3b9fb
Licoy <licoycn@gmail.com>
2018-04-18 18:06:49 +0800

    u

--- sbm.sql
+++ sbm.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50553
 File Encoding         : 65001
 
-Date: 2018-04-16 09:12:19
+Date: 2018-04-18 18:06:25
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,14 +23,19 @@ CREATE TABLE `permission` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   `permission` varchar(50) DEFAULT NULL,
+  `url` varchar(100) DEFAULT NULL,
+  `fid` int(11) DEFAULT NULL,
+  `sort` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of permission
 -- ----------------------------
-INSERT INTO `permission` VALUES ('1', '页面查看', 'page:select');
-INSERT INTO `permission` VALUES ('2', '页面删除', 'page:remove');
+INSERT INTO `permission` VALUES ('1', '页面查看', 'page:select', '/page/select/**', '0', '0');
+INSERT INTO `permission` VALUES ('2', '页面删除', 'page:remove', '/page/remove/**', '0', '0');
+INSERT INTO `permission` VALUES ('3', '用户锁定', 'system:user:lock', '/user/lock/**', '0', '0');
+INSERT INTO `permission` VALUES ('4', '用户解锁', 'system:user:unlock', '/user/unlock/**', '0', '0');
 
 -- ----------------------------
 -- Table structure for role
@@ -72,15 +77,33 @@ INSERT INTO `role_permission` VALUES ('3', '2', '1');
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) DEFAULT NULL,
-  `age` int(11) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
-  `role_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `username` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `age` int(11) NOT NULL,
+  `status` int(1) NOT NULL COMMENT '1',
+  `create_date` datetime NOT NULL,
+  PRIMARY KEY (`id`,`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'admin', '20', 'b9d11b3be25f5a1a7dc8ca04cd310b28', '1');
-INSERT INTO `user` VALUES ('2', 'user', '20', 'ad42c83ac4d3b86de14f207c46a0df0e', '2');
+INSERT INTO `user` VALUES ('1', 'admin', 'b9d11b3be25f5a1a7dc8ca04cd310b28', '20', '1', '2018-04-18 15:01:04');
+INSERT INTO `user` VALUES ('2', 'user', 'ad42c83ac4d3b86de14f207c46a0df0e', '20', '1', '2018-04-18 15:01:08');
+
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `rid` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`uid`,`rid`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
+INSERT INTO `user_role` VALUES ('1', '1', '1');
+INSERT INTO `user_role` VALUES ('2', '2', '2');
