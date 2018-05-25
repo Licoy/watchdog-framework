29e0bc6f06f2e010ae927f1746e9fa0890094088
Licoy <licoycn@gmail.com>
2018-05-25 14:18:36 +0800

    更新说明

--- src/main/java/cn/licoy/wdog/common/controller/CrudController.java
+++ src/main/java/cn/licoy/wdog/common/controller/CrudController.java
@@ -1,17 +1,6 @@
 package cn.licoy.wdog.common.controller;
 
-import cn.licoy.wdog.common.annotation.SysLogs;
-import cn.licoy.wdog.common.bean.ResponseCode;
-import cn.licoy.wdog.common.bean.ResponseResult;
 import cn.licoy.wdog.common.service.BaseService;
-import com.baomidou.mybatisplus.plugins.Page;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * @author Licoy
@@ -22,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
  * @param <UID> 对象ID
  * @param <FD> 查找DTO
  * @param <S> 服务对象
+ * <span> 此控制器只是针对于一般的操作设计，若需求不一样可自行定制 </span>
  */
 public interface CrudController<E,AD,UD,UID,FD,S extends BaseService<E,AD,UD,UID,FD>> extends
         CreateController<AD,S>,
